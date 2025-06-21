ed4dba1dc2f0c3440bb55414308c56f7dcae92f6
yaswanth2k <161770600+yash16062k@users.noreply.github.com>
2025-06-21 13:54:15 -0500

    Add files via upload

---
+++ NutrilensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrilens.nutrilens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrilensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrilensBackendApplication.class, args);
+	}
+
+}
